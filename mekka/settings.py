dfbb25e4b31cad2db208e136de423213bdbe728f
kaisertollo <tollosakho@gmail.com>
2023-07-16 23:35:48 +0000

    cors fix

--- mekka/settings.py
+++ mekka/settings.py
@@ -30,8 +30,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000"
 ]
 CORS_ALLOW_CREDENTIALS = True
-
-
+CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -43,12 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'facturations',
     'rest_framework',
+     'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
