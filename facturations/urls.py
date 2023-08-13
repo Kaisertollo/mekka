9049bd5d85441774988f425382c9d97f2a076358
kaisertollo <tollosakho@gmail.com>
2023-08-13 15:45:12 +0000

    send notification customer

--- facturations/urls.py
+++ facturations/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('customers', customer_api.CustomerAPI.as_view(), name='customers'),
     path('customer/<int:customer_id>',customer_api.CustomerById.as_view(), name='customer_by_id'),
     path('customers/login',CustomerApiLogin.as_view(), name='customer_api_login'),
+    path('send_customer_notification',SendCustomerNotificationApi.as_view(), name='customer_send_notification'),
     path('customer/token',customer_api.CustomerTokenAPI.as_view(), name='customer_api_token'),
 
     path('corporates', corporate_api.CorporateAPI.as_view(), name='corporates'),
--- facturations/views.py
+++ facturations/views.py
@@ -13,6 +13,18 @@ import string
 import facturations.api.invoices_api as invoice_api
 
 
+def send_notif(token,title,body,data_payload):
+    # Configure your Firebase Cloud Messaging server key
+    api_key = "AAAA0IjcuIQ:APA91bE75plgpm8K-9bzimA1GUY-fx7lu1AJwhaoJPW_5EOKAD6djPw-l1BTUHGrbMPdf7R_MH2VNYg0Trpbc9ZzYSnkxZSnMo44MHfagRjvOoqtNk12Ec8jFI570Fofht4CIEMEjCAh"
+
+    # Initialize the FCMNotification object
+    push_service = FCMNotification(api_key=api_key)
+    # Send a message to a specific device
+    registration_id =token
+    message_title = title
+    message_body = body
+    result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body,data_message=data_payload)
+    print(result)
 class AgentAPI(APIView):
     def get(self, request):
         agents = Agent.objects.all()
@@ -50,6 +62,16 @@ class CustomerApiLogin(APIView):
             code = generate_code()
             Send_wp(p,code)
             return Response({'id':c.id,'code':code})
+        else:
+            return Response({'id':0,'code':0})
+class SendCustomerNotificationApi(APIView):
+    def post(self, request):
+        p = request.data.get('phone')
+        message = request.data.get('msg')
+        c = Customer.objects.filter(phone = p).first()
+        if c:
+            send_notif(c.token,"Mekka Money",message,{})
+            return Response({'code':1})
         else:
             return Response({'id':0,'code':0})
         
